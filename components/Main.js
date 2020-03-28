9c4b3bc1fd3a706cc30bcd87a487baf910e6769c
mendsxx <61069014+mendsxx@users.noreply.github.com>
2020-03-28 18:08:27 -0300

    Ajustes, novos post e css

--- components/Main.js
+++ components/Main.js
@@ -3,65 +3,80 @@ import PropTypes from 'prop-types';
 class Main extends React.Component {
   render() {
 
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+    let close = <div className="close" onClick={() => { this.props.onCloseArticle() }}></div>
 
     return (
-      <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+      <div id="main" style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}>
 
-        <article id="blog" className={`${this.props.article === 'blog' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="blog" className={`${this.props.article === 'blog' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Blog</h2>
-        
-        <div id="post-01">
-          <h3>O que é Tech Stack?</h3>
+
+          {/* INICIO - ESTRUTURA POST */}
+          <div className="poststyle"> {/* Utilizar className*/}
+            <h3>O que é Tech Stack?</h3>
             <p>
               Conjunto de tecnologias que uma organização usa para criar um aplicativo Web ou móvel.
-              É uma combinação de linguagens de programação, estruturas, bibliotecas, padrões, servidores, 
-              soluções de UI / UX, software e ferramentas usadas por seus desenvolvedores. 
+              É uma combinação de linguagens de programação, estruturas, bibliotecas, padrões, servidores,
+              soluções de UI / UX, software e ferramentas usadas por seus desenvolvedores.
             </p>
+            <p className="postby"> {/* Utilizar className*/}
+                28/03/2020 - Lucas Martins - Adaptado de <a href="https://stackshare.io/stacks">stackshare.</a>
+            </p>
+          </div>
+          {/* FIM - ESTRUTURA POST */}
+
+          {/* INICIO - ESTRUTURA POST */}
+          <div className="poststyle"> {/* Utilizar className*/}
+            <h3>Seu Posto</h3>
             <p>
-                28/03/2020 - Lucas Martins - Adaptado de <a href="https://stackshare.io/stacks">stackshare.</a>  
+              Breve descrição sobre o tema
             </p>
-        </div>
-         
+            <p className="postby"> {/* Utilizar className*/}
+                00/00/0000 - Seu nome - Fonte <a href="link">nome.</a>
+            </p>
+          </div>
+          {/* FIM - ESTRUTURA POST */}
+
+          
 
           {close}
         </article>
 
-        <article id="techstack" className={`${this.props.article === 'techstack' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="techstack" className={`${this.props.article === 'techstack' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Tech Stack</h2>
-        <div>
-          <h3>Ferramentas utilizadas pelo nosso time! </h3>   
+          <div>
+            <h3>Ferramentas utilizadas pelo nosso time! </h3>
 
-            <img width="105px" src="../static/images/js.png"      alt="JavaScript"></img>
+            <img width="105px" src="../static/images/js.png" alt="JavaScript"></img>
 
-            <img width="105px" src="../static/images/next.png"    alt="Next.JS"></img>
+            <img width="105px" src="../static/images/next.png" alt="Next.JS"></img>
 
-            <img width="105px" src="../static/images/html.png"    alt="Html5"></img>
+            <img width="105px" src="../static/images/html.png" alt="Html5"></img>
 
-            <img width="105px" src="../static/images/css.png"     alt="Css"></img>
+            <img width="105px" src="../static/images/css.png" alt="Css"></img>
 
-            <img width="105px" src="../static/images/aws.png"     alr="Aws"></img>
+            <img width="105px" src="../static/images/aws.png" alr="Aws"></img>
 
             <img width="105px" src="../static/images/angular.png" alt="Angular"></img>
 
-            <img width="105px" src="../static/images/ionic.png"   alt="Ionic"></img>  
-        
-        </div>          
+            <img width="105px" src="../static/images/ionic.png" alt="Ionic"></img>
 
-         {close}
+          </div>
+
+          {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Quem Somos</h2>
-        
+
           <p>Me edita pra alterar o Quem Somos</p>
 
-         {close}
+          {close}
         </article>
 
-        <article id="projetos" className={`${this.props.article === 'projetos' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="projetos" className={`${this.props.article === 'projetos' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Projetos Pessoais</h2>
-        
+
           <p>Me edita pra alterar o Projetos Pessoais</p>
 
           {close}
