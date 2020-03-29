a51e72c102c236eb5b30a69395dfd6ef5f02d0ca
danielmoreiraunifil <daniel.moreira@edu.unifil.br>
2020-03-28 21:14:57 -0300

    quem somos
    28-03-2020

--- components/Main.js
+++ components/Main.js
@@ -65,25 +65,35 @@ class Main extends React.Component {
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Quem Somos</h2>
-
-          <p>
+         <div className="poststyle">
+          
             <h2>Lucas Mendes</h2>
-             <span className="image main"><img src="/static/images/lucas.jpg" alt="" /></span>
+             <img width="250px" src="/static/images/lucas.jpg" alt="" />
           <p>Lucas Mendes, Londrinense, 20 anos, cursando Engenharia de Software - UNIFIL, tem aprofundado seus conhecimentos em desenvolvimento Web e Mobile utilizando ferramentas como: NodeJS, JavaScript, NextJS, ReactJS, Angular, Ionic, HMTL, CSS, entre outras. Trabalha na BRID Soluções na área de Qualidade de Dados como analista, densenvolvendo em ferramentas QlikView, QlikSense, 
               Power BI e Alteryx.
                Para mais informações do Lucas:  https://www.linkedin.com/in/lucas-m-martins/ 
                                                 https://github.com/mendsxx/BlogProducao  
-          ______________________________________________________________________
+         </p>
+         </div>                                      
+         
+         <div className="poststyle">
           <h2>Daniel Moreira</h2>
-            <span className="image main"><img src="/static/images/perfil.jpg" alt="" /></span>
+            <span ><img src="/static/images/perfil.jpg" alt="" /></span>
                 <p> Daniel Moreira, Sertanopolense, 18 anos, cursando Engenharia de Software - UNIFIL, tem aprofundado seus conhecimentos em desenvolvimento Web  utilizando ferramentas como: Angular, NextJS, ReactJS, NodeJS, HMTL, entre outras. Trabalha na JMTI Tecnologia e Soluções e na SDR Viagens.
                 Para mais informações do Daniel: https://www.linkedin.com/in/daniel-moreira-68177315a/
                                                  https://github.com/danielmoreiraunifil 
                  </p>
-          </p>
-          ______________________________________________________________________
+          </div>
+          
+          <div className="poststyle">
           <h2>Felipe Lonni</h2> 
-         </p>
+             <span ><img src="/static/images/felipe.jpg" alt="" /></span>
+               <p>
+                 Felipe Lonni, Cambeense, 19 anos, cursando Engenharia de Software - UNIFIL, tem aprofundado seus conhecimentos em desenvolvimento utilizando ferramentas como: Java, NextJS, HTML, entre outras. Trabalha no Sine de Cambé.
+                    Para mais informações do Felipe: 
+               </p>
+         
+           </div>
           {close}
         </article>
 
